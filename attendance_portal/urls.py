2eaefb5090f9d7b2da498ecff846ef0b51774479
Dhruvinsinh Rathod <100965714+DhruvinRathod@users.noreply.github.com>
2026-08-30 23:13:34 +0200

    Expose one-click recruiter demo login

--- attendance_portal/urls.py
+++ attendance_portal/urls.py
@@ -1,11 +1,21 @@
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
-from attendance.views import logout_view
+
+from attendance.views import demo_login, logout_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="registration/login.html",
+            extra_context={"demo_mode": settings.DEMO_MODE},
+        ),
+        name="login",
+    ),
+    path("demo-login/", demo_login, name="demo_login"),
     path("logout/", logout_view, name="logout"),
     path("", include("attendance.urls")),
 ]
